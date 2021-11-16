39992d0dc66f22e99e974b459a07fef6655640fe
Naoki Fujita <fujitanaoki0718@gmail.com>
2021-11-17 01:48:44 +0900

    feat: onscrollベースでの実装を検討

--- pages/sandboxes/scroll_bars.tsx
+++ pages/sandboxes/scroll_bars.tsx
@@ -1,6 +1,5 @@
-import { useRef } from 'react';
 import styles from './scroll_bars.module.scss'; 
-import { useScrollPosition } from 'react-use-scroll-position';
+import throttle from 'lodash/throttle';
 
 const FakeContent: React.FC<{prefix: string}> = (props) => (
   <div style={{backgroundColor: '#def'}} >
@@ -8,32 +7,29 @@ const FakeContent: React.FC<{prefix: string}> = (props) => (
   </div>
 );
 
+const loggingToBeThrottled = ()=>console.log('abc');
+
 const ScrollBars: React.VFC = () => {
-  const scrolledContentRef = useRef();
-  useScrollPosition(({ currPos }) => {
-    console.log(currPos);
-  }, [], scrolledContentRef
-  );
   return (
     <article style={{height: '100vh', width: '100vw'}}>
       <div style={{height: '100%', display: 'flex'}}>
 
-        <div style={{height: '100%',  overflowY: 'hidden', paddingRight: '1rem'}}>
+        <div style={{height: '100%',  overflowY: 'hidden', paddingRight: '1rem', flexShrink: 0}}>
           <div style={{height: '100%', width: '300px', overflowY: 'scroll',  overscrollBehavior: 'contain'}}>
             <FakeContent prefix="パターン1(標準)"/>
           </div>
         </div>
 
-        <div style={{height: '100%',  overflowY: 'hidden'}}>
-          <div className={styles['scroll-area']}>
+        <div style={{height: '100%',  overflowY: 'hidden', flexShrink: 0}}>
+          <div onScroll={throttle(loggingToBeThrottled, 10)} className={styles['scroll-area']}>
             <div className={styles['scroll-thumb']}/>
-            <div ref={scrolledContentRef} style={{backgroundColor: '#def'}} >
+            <div style={{backgroundColor: '#def'}} >
               {`パターン2(カスタムバー): ${Array.from({length: 1000}, (_v, k) => k).map(n=>n.toString()).join()}`}
             </div>
           </div>
         </div>
 
-        <div style={{height: '100%',  width: '300px'}}>
+        <div style={{height: '100%',  width: '300px', flexShrink: 0}}>
         </div>
 
       </div>
