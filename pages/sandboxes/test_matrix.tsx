12159ef9987ba548051c15bd56d504cb0644f1f7
Naoki Fujita <fujitanaoki0718@gmail.com>
2022-03-31 17:33:10 +0900

    feat: componentのテスト用

--- pages/sandboxes/test_matrix.tsx
+++ pages/sandboxes/test_matrix.tsx
@@ -1,50 +1,11 @@
 import { useState } from 'react';
-
-const codeTypes = ['base64', 'binary'] as const;
-type CodeType = typeof codeTypes[number];
-
-const identityString = (s: string)=>s;
-const encodeDispatcher: (args:Readonly<{codeType: CodeType}>)=>((data:string)=>string) = ({codeType}) =>{
-  switch(codeType){
-    case 'base64':
-      return btoa;
-  }
-  return identityString;
-};
-
-const decodeDispatcher: (args:Readonly<{codeType: CodeType}>)=>((data:string)=>string) = ({codeType}) =>{
-  switch(codeType){
-    case 'base64':
-      return atob;
-  }
-  return identityString;
-};
-
+import { RenderNewLines } from '../../components/RenderNewLines';
 const ScrollBars: React.FC = () => {
-  const [decoded, setDecoded] = useState('');
-  const [encoded, setEncoded] = useState('');
-  const [codeType, setCodeType] = useState<CodeType>('base64');
-  const onEncode=()=>{
-    const encoder = encodeDispatcher({codeType});
-    setEncoded(encoder(decoded));    
-  };
-  const onDecode = ()=>{
-    const decoder = decodeDispatcher({codeType});
-    setDecoded(decoder(encoded));    
-  };
-
+  const [str, setStr] = useState('');
   return (
     <main style={{height: '100vh', width: '100vw', padding: '1rem'}}>
-      <div style={{display: 'flex', height: '5%', padding: '0.5rem', justifyContent:'center', alignItems:'center'}}>
-        <span style={{paddingRight: '0.5rem'}}>encoding type: </span>
-        <select value={codeType} onChange={e=>setCodeType(e.target.value as CodeType)}>
-          {codeTypes.map(c=>(
-            <option key={c} value={c} style={{padding: '0.5rem'}}>
-              {c}            
-            </option>
-          ))}
-        </select>
-      </div>
+      <textarea value={str} onChange={(event)=>{ setStr(event.target.value); }}/>
+      <RenderNewLines rawString={str}/>
     </main>
   ); };
 export default ScrollBars;
